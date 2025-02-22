a25e059884ebeb4331388e3be173324db41e1229
Anushka Bhandarkar <anushkabhandarkar25@gmail.com>
2025-02-22 16:35:58 +0530

    Initial commit - AI Interview Prep App

---
+++ app/page.js
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+   <div>
+     <h1>hello this is the blank page</h1>
+     <Button>experimentation</Button>
+   </div>
+  );
+}
