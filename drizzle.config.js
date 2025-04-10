67675315293f464130dd4eda938ae9ad4a7261e7
Anushka Bhandarkar <anushkabhandarkar25@gmail.com>
2025-04-10 23:01:44 +0530

    Moved PostgreSQL URI to .env and secured config

--- drizzle.config.js
+++ drizzle.config.js
@@ -1,3 +1,4 @@
+import "dotenv/config";
 /** @type { import("drizzle-kit").Config } */
 
 export default {
@@ -5,6 +6,6 @@ export default {
   schema: "./utils/schema.js",
   out: "./drizzle",
   dbCredentials: {
-    url: "postgresql://neondb_owner:npg_3c2xMzTIFDaN@ep-still-mountain-a1pnuw51-pooler.ap-southeast-1.aws.neon.tech/mockmate?sslmode=require",
+    url: process.env.NEXT_PUBLIC_DRIZZLE_DB_URL,
   },
 };
